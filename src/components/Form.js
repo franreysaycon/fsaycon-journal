fb039dd68102493272723639365fcb277f43d8fb
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2020-11-22 18:24:42 +0800

    Finishibushi

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,5 @@
 import { Button, Stack, Textarea } from '@chakra-ui/core'
 import React, { forwardRef } from 'react'
-import PropTypes from 'prop-types'
 
 const Form = ({submit} , ref) => (
     <Stack spacing={3} mr={3} ml={3}>
@@ -8,9 +7,5 @@ const Form = ({submit} , ref) => (
         <Button variantColor="blue" alignSelf="flex-end" onClick={submit}>Submit</Button>
     </Stack>
 )
-
-Form.propTypes = {
-    submit: PropTypes.func.isRequired,
-}
 
 export default forwardRef(Form)
